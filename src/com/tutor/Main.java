7865da90c0333796954100f4a2666836ed77512b
aiusquare <ahmadibrahimusmanmkk@gmail.com>
2020-03-10 15:06:40 +0100

    array sorting

--- src/com/tutor/Main.java
+++ src/com/tutor/Main.java
@@ -1,9 +1,16 @@
 package com.tutor;
 
+import java.util.*;
+
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] arr= {3,4,1,5,2};
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        
     }
     
 }
